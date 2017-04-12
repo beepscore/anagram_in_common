a986f9517eb94bb60278b19ad9a3adcaf92fc192
beepscore <support@beepscore.com>
2017-04-11 23:11:07 -0700

    Add anagram_in_common as a static method. Import and usage seems verbose! TODO: Consider remove class AnagramInCommon, just use it’s methods.

--- anagram_in_common/anagram_in_common.py
+++ anagram_in_common/anagram_in_common.py
@@ -7,3 +7,10 @@ class AnagramInCommon:
 Returns minimum number of letters you must remove from each a and b so that the remaining letters are anagrams.
 Assume for purposes of this puzzle, an "anagram" does not need to be a valid English word.
 It just represents a sequence of letters."""
+
+    @staticmethod
+    def anagram_in_common(a: str, b: str):
+        """result is a tuple of the form
+        (anagram from a, letters removed from a, letters removed from b)
+        """
+        return ('ac', 1, 2)
--- tests/test_anagram_in_common.py
+++ tests/test_anagram_in_common.py
@@ -3,9 +3,15 @@
 import unittest
 from anagram_in_common import anagram_in_common
 
-
 class TestAnagramInCommon(unittest.TestCase):
 
     def test_anagram_in_common(self):
-        self.assertTrue(True)
+        a = "abc"
+        b = "ecad"
+
+        # call method under test
+        result = anagram_in_common.AnagramInCommon.anagram_in_common(a, b)
+        # result is a tuple of the form
+        # (anagram from a, letters removed from a, letters removed from b)
+        self.assertEqual(result, ('ac', 1, 2))
 
