6ad0d617e98e0aee9e60399d59da3dd9cd0a486f
beepscore <support@beepscore.com>
2017-04-11 22:54:17 -0700

    Add anagram_in_common.py and test_anagram_in_common.py

---
+++ anagram_in_common/anagram_in_common.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+class AnagramInCommon:
+    """ Given two strings a and b, each contains only ascii lowercase a-z.
+
+Returns minimum number of letters you must remove from each a and b so that the remaining letters are anagrams.
+Assume for purposes of this puzzle, an "anagram" does not need to be a valid English word.
+It just represents a sequence of letters."""
---
+++ tests/test_anagram_in_common.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import unittest
+from anagram_in_common import anagram_in_common
+
+
+class TestAnagramInCommon(unittest.TestCase):
+
+    def test_anagram_in_common(self):
+        self.assertTrue(True)
+
